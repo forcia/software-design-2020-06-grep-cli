a08e63bba9a6e83a4c996b2a6d5bbf3bb3b97dc1
matsumoto <kentaro_matsumoto@forcia.com>
2020-03-15 16:01:51 +0900

    clapとregexを追加し、正規表現モードとテキストモードの切り替えを実現

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,65 @@
+use clap::{crate_authors, crate_version, App, Arg};
+use regex::Regex;
 use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
 
-fn check(line:&str, pattern:&str)->bool{
+#[derive(Debug)]
+enum MatchMode {
+    ExtendedRegexp,
+    FixedStrings,
+}
+
+fn match_pattern(line: &str, pattern: &str) -> bool {
+    let re = Regex::new(pattern).unwrap();
+    re.is_match(line)
+}
+fn match_fix_string(line: &str, pattern: &str) -> bool {
     line.contains(pattern)
 }
 
 fn main() {
+    let matches = App::new("grep")
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about("Search for PATTERNS in each FILE.")
+        .arg(
+            Arg::with_name("fixed-strings")
+                .short("F")
+                .long("fixed-strings")
+                .help("PATTERNS are strings"),
+        )
+        .arg(
+            Arg::with_name("PATTERNS")
+                .help("use PATTERNS for matching")
+                .required(true)
+                .index(1),
+        )
+        .arg(
+            Arg::with_name("FILE")
+                .help("take PATTERNS from FILE")
+                .required(true)
+                .index(2),
+        )
+        .get_matches();
+
+    let match_mode = if matches.is_present("fixed-strings") {
+        MatchMode::FixedStrings
+    } else {
+        MatchMode::ExtendedRegexp
+    };
+
+    let pattern = match matches.value_of("PATTERNS") {
+        Some(p) => p,
+        None => "",
+    };
+    let file_path = match matches.value_of("FILE") {
+        Some(f) => f,
+        None => "",
+    };
+
     // Create a path to the desired file
-    let path = Path::new("./book/CONTRIBUTING.md");
+    let path = Path::new(&file_path);
     let display = path.display();
 
     // Open the path in read-only mode, returns `io::Result<File>`
@@ -22,27 +73,42 @@ fn main() {
     match file.read_to_string(&mut s) {
         Err(why) => panic!("couldn't read {}: {}", display, why.to_string()),
         Ok(_) => {
-            // "you"が含まれている行のみを表示する
-            for line in s.lines(){
-                if check(line, "you") {
-                    println!("{}", line);
+            for line in s.lines() {
+                match match_mode {
+                    MatchMode::ExtendedRegexp => {
+                        if match_pattern(line, &pattern) {
+                            println!("{}", line);
+                        }
+                    }
+                    MatchMode::FixedStrings => {
+                        if match_fix_string(line, &pattern) {
+                            println!("{}", line);
+                        }
+                    }
                 }
             }
-        },
+        }
     }
-
-
-    // `file` goes out of scope, and the "hello.txt" file gets closed
 }
 
 #[cfg(test)]
-mod test{
+mod test {
     use super::*;
 
     #[test]
-    fn test_check(){
-        assert_eq!(true, check("abcdefg", "c"));
-        assert_eq!(true, check("abcdefg", "fg"));
-        assert_eq!(false, check("abcdefg", "Z"));
+    fn test_match_pattern() {
+        assert_eq!(true, match_pattern("abcdefg", "c"));
+        assert_eq!(true, match_pattern("abcdefg", "fg"));
+        assert_eq!(false, match_pattern("abcdefg", "Z"));
+        assert_eq!(true, match_pattern("abcdefg", "a.c"));
+        assert_eq!(true, match_pattern("aaa bbb", "a+.b+"));
+        assert_eq!(true, match_pattern("aBc", "[aA][bB][cC]"));
+    }
+    #[test]
+    fn test_match_fix_string() {
+        assert_eq!(true, match_fix_string("abcdefg", "c"));
+        assert_eq!(true, match_fix_string("abcdefg", "fg"));
+        assert_eq!(false, match_fix_string("abcdefg", "Z"));
+        assert_eq!(false, match_fix_string("abcdefg", "a.c"));
     }
 }
